45e5b495616c57b0162b387413e93a5514be2f44
deiveehan <deiveehan@gmail.com>
2018-05-19 01:05:40 -0500

    Fragment activity communication files commit

--- fragmentactivitycommunication/app/src/main/java/io/ctrlspace/fragment_activity_communication/MainActivity.java
+++ fragmentactivitycommunication/app/src/main/java/io/ctrlspace/fragment_activity_communication/MainActivity.java
@@ -1,13 +1,39 @@
 package io.ctrlspace.fragment_activity_communication;
 
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import io.ctrlspace.fragment_activity_communication.MessageFragment.OnMessageReadListener;
+
+public class MainActivity extends AppCompatActivity implements OnMessageReadListener{
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(findViewById(R.id.fragment_container) != null) {
+            if(savedInstanceState != null) {
+                return;
+            }
+
+
+            MessageFragment messageFragment = new MessageFragment();
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, messageFragment, null);
+            fragmentTransaction.commit();
+        }
+
+
+    }
+
+    @Override
+    public void onMessageRead(String message) {
+        textView = (TextView) findViewById(R.id.txt_display_message);
+        textView.setText(message);
     }
 }
